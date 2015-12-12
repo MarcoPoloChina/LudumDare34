ef3e24b84db338407f91303d868692dbfd2fc467
derek <dwm55@drexel.edu>
2015-12-12 12:06:12 -0500

    autocommit for ludumdare

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
+
 }
 
 function create() {
+	player = new Phaser.Rectangle(0,0,50,50);
 }
 
 function update() {
